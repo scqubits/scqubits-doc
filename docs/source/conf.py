804f220f8e3cb5b78f522e9170ef9e259b2e32c0
saipavanc <saipavanchitta1998@gmail.com>
2024-12-15 03:00:49 -0600

    Attempt to fix mathjax output compilation for Circuit output

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,21 @@ extensions = [
     "sphinx_copybutton"
 ]
 
+mathjax3_config = {
+    'tex': {
+        'inlineMath': [['$', '$'], ['\\(', '\\)']],
+        'displayMath': [['$$', '$$'], ['\\[', '\\]']],
+        'processEscapes': True,
+        'processEnvironments': True
+    },
+    'options': {
+        'ignoreHtmlClass': 'tex2jax_ignore|mathjax_ignore|document',
+        'processHtmlClass': 'tex2jax_process|mathjax_process'
+    },
+    'loader': {'load': ['[tex]/ams']}
+}
 
+mathjax_path = "https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"
 # -- Internationalization ------------------------------------------------
 # specifying the natural language populates some key tags
 language = "en"
